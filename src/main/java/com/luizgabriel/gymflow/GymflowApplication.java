4a61b70fe2eda3e375ad2fd84ee2f50f01e15561
Luiz Gabriel <luiz.gabrielcb2006@gmail.com>
2026-04-30 12:03:52 -0300

    chore: initial project setup

---
+++ src/main/java/com/luizgabriel/gymflow/GymflowApplication.java
@@ -0,0 +1,13 @@
+package com.luizgabriel.gymflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymflowApplication.class, args);
+	}
+
+}

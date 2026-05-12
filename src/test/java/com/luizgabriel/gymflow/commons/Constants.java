2cd9ac2784d60e39336ca654154e8792af50fef3
Luiz Gabriel <luiz.gabrielcb2006@gmail.com>
2026-05-12 00:01:09 -0300

    test: add integration tests for ExerciseController and configure test environment

--- src/test/java/com/luizgabriel/gymflow/commons/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.luizgabriel.gymflow.commons;
-
-
-public class Constants {
-    public static final String BASE_URI = "http://localhost:";
-    public static final String REGULAR_USERNAME = "user.test@gmail.com";
-    public static final String ADMIN_USERNAME = "admin.test@gmail.com";
-    public static final String PASSWORD = "test";
-}
---
+++ src/test/java/com/luizgabriel/gymflow/config/IntegrationTestConfig.java
@@ -0,0 +1,42 @@
+package com.luizgabriel.gymflow.config;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("itest")
+public abstract class IntegrationTestConfig {
+    public static final PostgreSQLContainer<?> POSTGRES_CONTAINER;
+
+    static {
+        POSTGRES_CONTAINER = new PostgreSQLContainer<>(DockerImageName.parse("postgres:17.9"))
+                .withDatabaseName("gymflow");
+
+        POSTGRES_CONTAINER.start();
+    }
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+        RestAssured.basePath = "/gymflow/v1";
+    }
+
+    @DynamicPropertySource
+    static void overrideDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRES_CONTAINER::getPassword);
+    }
+}
